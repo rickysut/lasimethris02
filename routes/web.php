c2bf930a97e69432ac22b28df53d894f5193b616
RickySBahaso <ricky@sali-evo.com>
2023-05-20 20:48:32 +0700

    route , delete duplikat route

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
 	Route::post('posts/{post}/star', 'StarredPostController@star')->name('posts.star');
 	Route::delete('posts/{post}/unstar', 'StarredPostController@unstar')->name('posts.unstar');
 
-	//posts categories
+	//categories
 	Route::resource('categories', 'CategoryController');
 
 	//messenger
@@ -149,151 +149,6 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
 		// Route::get('masterpoktan', 'MasterpoktanController@index')->name('masterpoktan');
 	});
 
-
-	Route::resource('riphAdmin', 'RiphAdminController');
-
-	//skl-admin
-	Route::resource('skl', 'AdminSKLController');
-
-	// landing
-	Route::get('/', 'HomeController@index')->name('home');
-	// Dashboard
-	Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
-	Route::get('/dashboard/map', 'DashboardController@map')->name('dashboard.map');
-	// Permissions
-	Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
-	Route::resource('permissions', 'PermissionsController');
-
-	// Roles
-	Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
-	Route::resource('roles', 'RolesController');
-
-	// Users
-	Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
-	Route::resource('users', 'UsersController');
-
-	// Audit Logs
-	Route::resource('audit-logs', 'AuditLogsController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
-
-	Route::get('global-search', 'GlobalSearchController@search')->name('globalSearch');
-
-	Route::get('profile', 'ProfileController@index')->name('profile.show');
-	Route::post('profile', 'ProfileController@store')->name('profile.store');
-
-	//posts
-	Route::put('posts/{post}/restore', 'PostsController@restore')->name('posts.restore');
-	Route::resource('posts', 'PostsController');
-	Route::get('allblogs', 'PostsController@allblogs')->name('allblogs');
-	Route::post('posts/{post}/star', 'StarredPostController@star')->name('posts.star');
-	Route::delete('posts/{post}/unstar', 'StarredPostController@unstar')->name('posts.unstar');
-
-	//posts categories
-	Route::resource('categories', 'CategoryController');
-
-	//messenger
-	Route::get('messenger', 'MessengerController@index')->name('messenger.index');
-	Route::get('messenger/create', 'MessengerController@createTopic')->name('messenger.createTopic');
-	Route::post('messenger', 'MessengerController@storeTopic')->name('messenger.storeTopic');
-	Route::get('messenger/inbox', 'MessengerController@showInbox')->name('messenger.showInbox');
-	Route::get('messenger/outbox', 'MessengerController@showOutbox')->name('messenger.showOutbox');
-	Route::post('messenger/{topic}/update', 'MessengerController@updateTopic')->name('messenger.updateTopic');
-	Route::get('messenger/{topic}', 'MessengerController@showMessages')->name('messenger.showMessages');
-	Route::delete('messenger/{topic}', 'MessengerController@destroyTopic')->name('messenger.destroyTopic');
-	Route::post('messenger/{topic}/reply', 'MessengerController@replyToTopic')->name('messenger.reply');
-	Route::get('messenger/{topic}/reply', 'MessengerController@showReply')->name('messenger.showReply');
-
-	//verifikasi
-	Route::get('dir_check_b', 'MessengerController@showReply')->name('verifikasi.dir_check_b');
-	Route::get('dir_check_c', 'MessengerController@showReply')->name('verifikasi.dir_check_c');
-
-	//user task
-	Route::group(['prefix' => 'task', 'as' => 'task.'], function () {
-
-
-		Route::get('pull', 'PullRiphController@index')->name('pull');
-		Route::get('getriph', 'PullRiphController@pull')->name('pull.getriph');
-		Route::post('pull', 'PullRiphController@store')->name('pull.store');
-
-		Route::get('commitment', 'CommitmentController@index')->name('commitment');
-		Route::get('commitment/{pullriph}', 'CommitmentController@show')->name('commitment.show');
-		Route::delete('commitment/{pullriph}', 'CommitmentController@destroy')->name('commitment.destroy');
-		Route::post('commitment/unggah', 'CommitmentController@store')->name('commitment.store');
-		Route::delete('commitmentmd', 'CommitmentController@massDestroy')->name('commitment.massDestroy');
-
-		// pengajuan
-		Route::resource('pengajuan', 'PengajuanController');
-		Route::delete('pengajuan/destroy', 'PengajuanController@massDestroy')->name('pengajuan.massDestroy');
-
-		// kelompoktani
-		Route::get('kelompoktani', 'KelompoktaniController@index')->name('kelompoktani');
-		Route::get('kelompoktani/{noriph}/edit', 'KelompoktaniController@edit')->name('kelompoktani.edit');
-		Route::get('kelompoktani/create', 'KelompoktaniController@create')->name('kelompoktani.create');
-		Route::get('kelompoktani/{noriph}', 'KelompoktaniController@show')->name('kelompoktani.show');
-		Route::delete('kelompoktanimd', 'KelompoktaniController@massDestroy')->name('kelompoktani.massDestroy');
-		Route::get('petani/{id_poktan}', 'KelompoktaniController@showtani')->name('kelompoktani.showtani');
-
-		// daftar pks
-		Route::get('pks/create/{noriph}/{poktan}', 'PksController@create')->name('pks.create');
-		Route::delete('pksmd', 'PksController@massDestroy')->name('pks.massDestroy');
-		Route::resource('pks', 'PksController')->except(['create']);
-
-
-		Route::resource('skl', 'SklController');
-
-		//berkas
-		Route::get('berkas', 'BerkasController@indexberkas')->name('berkas');
-
-		//galeri
-		Route::get('galeri', 'BerkasController@indexgaleri')->name('galeri');
-
-		//template
-		Route::delete('template/destroy', 'BerkasController@massDestroy')->name('template.massDestroy');
-		Route::get('template/create', 'BerkasController@createtemplate')->name('template.create');
-		Route::delete('template/{id}', 'BerkasController@destroytemplate')->name('template.destroy');
-		Route::post('template', 'BerkasController@storetemplate')->name('template.store');
-		//Route::get('template/{berkas}', 'BerkasController@showtemplate')->name('template.show');
-		Route::get('template/{berkas}/edit', 'BerkasController@edittemplate')->name('template.edit');
-		Route::put('template/{berkas}', 'BerkasController@updatetemplate')->name('template.update');
-		Route::get('template', 'BerkasController@indextemplate')->name('template');
-
-		//route for backdate (v2)
-		//commitment (v2)
-		Route::resource('commitments', 'CommitmentBackdateController');
-		Route::get('commitments/{commitments}/penangkar', 'CommitmentBackdateController@penangkar')->name('commitments.penangkar');
-		Route::get('commitments/{commitments}/read', 'CommitmentBackdateController@read')->name('commitments.read');
-		Route::get('commitments/{commitments}/pksmitra', 'CommitmentBackdateController@pksmitra')->name('commitments.pksmitra');
-
-		//pengajuan v2
-		Route::put('commitments/{commitments}/createpengajuan', 'CommitmentBackdateController@createpengajuan')->name('commitments.createpengajuan');
-		Route::put('commitments/{commitments}/pengajuanulang', 'CommitmentBackdateController@pengajuanulang')->name('commitments.pengajuanulang');
-		Route::get('pengajuanv2', 'PengajuanV2Controller@index')->name('pengajuanv2.index');
-
-		//pks mitra v2
-		Route::resource('pksmitra', 'PksMitraController');
-
-		//route for penangkar mitra v2
-		// Route::resource('penangkarmitra', 'PenangkarMitraController');
-		Route::get('/penangkarmitra/{penangkarmitra}', 'PenangkarMitraController@show')->name('penangkarmitra.show');
-		Route::post('/penangkarmitra/store', 'PenangkarMitraController@store')->name('penangkarmitra.store');
-		Route::put('/penangkarmitra/{penangkarmitra}', 'PenangkarMitraController@update')->name('penangkarmitra.update');
-		Route::delete('/penangkarmitra/{penangkarmitra}', 'PenangkarMitraController@destroy')->name('penangkarmitra.destroy');
-
-		//anggota mitra pks v2
-		Route::get('/anggotamitra/{anggotamitra}', 'AnggotaMitraController@show')->name('anggotamitra.show');
-		Route::post('/anggotamitra/store', 'AnggotaMitraController@store')->name('anggotamitra.store');
-		Route::put('/anggotamitra/{anggotamitra}/update', 'AnggotaMitraController@update')->name('anggotamitra.update');
-		Route::delete('/anggotamitra/{anggotamitra}', 'AnggotaMitraController@destroy')->name('anggotamitra.destroy');
-
-		//kelompok tani v2
-		Route::resource('masterpoktan', 'MasterpoktanController');
-		Route::resource('anggotapoktan', 'AnggotaPoktanController');
-		Route::resource('masterpenangkar', 'MasterPenangkarController');
-		// Route::get('masterpoktan', 'MasterpoktanController@index')->name('masterpoktan');
-
-
-	});
-
-
 	Route::resource('riphAdmin', 'RiphAdminController');
 
 	//skl-admin
@@ -308,10 +163,7 @@ Route::group(['prefix' => 'verification', 'as' => 'verification.', 'namespace' =
 });
 
 
-
 Route::group(['prefix' => 'profile', 'as' => 'profile.', 'namespace' => 'Auth', 'middleware' => ['auth']], function () {
-
-
 
 	// Change password
 	if (file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php'))) {
